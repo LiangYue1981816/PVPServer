3e6ade02b97eda3011bf3ba067fdf7bf45dbd24f
LiangYue <liangyue1981816@hotmail.com>
2016-03-08 14:31:54 +0800

    - 调整目录结构

---
+++ Proto/proto.bat
@@ -0,0 +1,9 @@
+set PROTOC=..\Server\ProtoBuf\Tools\protoc.exe
+set PROTOCSHARP=..\Server\ProtoBuf\Tools\protocsharp.exe
+
+for %%i in (*.proto) do (
+	%PROTOC% -I=.\ --cpp_out=.\cpp .\%%i
+	%PROTOCSHARP% -i:%%i -o:.\csharp\%%i.cs
+)
+
+pause
