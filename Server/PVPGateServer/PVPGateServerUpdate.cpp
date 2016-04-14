338279ad882b24f14dfee60cec2eb9bec473dc0d
LiangYue <liangyue1981816@hotmail.com>
2016-04-14 16:13:26 +0800

    + 添加协议代码

--- Server/PVPGateServer/PVPGateServerUpdate.cpp
+++ Server/PVPGateServer/PVPGateServerUpdate.cpp
@@ -6,11 +6,15 @@
 //
 void CPVPGateServer::OnUpdateMatch(DWORD dwDeltaTime)
 {
+	static DWORD dwTime = 0;
+	static const float timeOut = 100.0f;
+	static const int maxGamePlayers = 6;
+	static const DWORD dwMinEvaluation = 0;
+	static const DWORD dwMaxEvaluation = 10000;
+
 	//
 	// 1. 更新即时检查
 	//
-	static DWORD dwTime = 0;
-
 	dwTime += dwDeltaTime;
 
 	if (dwTime < 1000) {
@@ -22,4 +26,9 @@ void CPVPGateServer::OnUpdateMatch(DWORD dwDeltaTime)
 	//
 	// 2. 匹配计算
 	//
+	for (PlayerEvaluationMap::iterator itPlayerMap = m_evaluations.begin(); itPlayerMap != m_evaluations.end(); ++itPlayerMap) {
+		for (std::map<DWORD, PlayerStatus>::iterator itPlayer = itPlayerMap->second.begin(); itPlayer != itPlayerMap->second.end(); ++itPlayer) {
+			itPlayer->second.timeout += 1.0f;
+		}
+	}
 }
