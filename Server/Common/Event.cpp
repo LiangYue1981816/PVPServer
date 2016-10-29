cdbec43be98cef585ee8a025162ab983cf170da1
LiangYue <LiangYue1981816@Hotmail.com>
2016-10-29 23:34:35 +0800

    - 调整代码

--- Server/Common/Event.cpp
+++ Server/Common/Event.cpp
@@ -1,55 +1,59 @@
 #include "Event.h"
 
 
-//
-// 初始化
-//
-_ServerExport void event_init(event_t *event)
+void event_init(event_t *event)
 {
-	event->flag = FALSE;
+#if defined _PLATFORM_WINDOWS_
+	event->hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
+#else
+	event->flag = 0;
 	pthread_cond_init(&event->cond, NULL);
 	pthread_mutex_init(&event->mutex, NULL);
+#endif
 }
 
-//
-// 销毁
-//
-_ServerExport void event_destroy(event_t *event)
+void event_destroy(event_t *event)
 {
+#if defined _PLATFORM_WINDOWS_
+	CloseHandle(event->hEvent);
+#else
 	pthread_cond_destroy(&event->cond);
 	pthread_mutex_destroy(&event->mutex);
+#endif
 }
 
-//
-// 触发
-//
-_ServerExport void event_set(event_t *event)
+void event_set(event_t *event)
 {
+#if defined _PLATFORM_WINDOWS_
+	SetEvent(event->hEvent);
+#else
 	pthread_mutex_lock(&event->mutex);
 	{
-		event->flag = TRUE;
+		event->flag = 1;
 		pthread_cond_signal(&event->cond);
 	}
 	pthread_mutex_unlock(&event->mutex);
+#endif
 }
 
-//
-// 重置
-//
-_ServerExport void event_reset(event_t *event)
+void event_reset(event_t *event)
 {
+#if defined _PLATFORM_WINDOWS_
+	ResetEvent(event->hEvent);
+#else
 	pthread_mutex_lock(&event->mutex);
 	{
-		event->flag = FALSE;
+		event->flag = 0;
 	}
 	pthread_mutex_unlock(&event->mutex);
+#endif
 }
 
-//
-// 等待
-//
-_ServerExport void event_wait(event_t *event)
+void event_wait(event_t *event)
 {
+#if defined _PLATFORM_WINDOWS_
+	WaitForSingleObject(event->hEvent, INFINITE);
+#else
 	pthread_mutex_lock(&event->mutex);
 	{
 		while (!event->flag) {
@@ -57,18 +61,31 @@ _ServerExport void event_wait(event_t *event)
 		}
 	}
 	pthread_mutex_unlock(&event->mutex);
+#endif
 }
 
-_ServerExport void event_timedwait(event_t *event, int sec, int nsec)
+int event_timedwait(event_t *event, int msec)
 {
+	int rcode;
+
+#if defined _PLATFORM_WINDOWS_
+	rcode = WaitForSingleObject(event->hEvent, msec);
+#else
+	int nsec;
+	struct timeval now;
+	struct timespec abstime;
+
+	gettimeofday(&now, NULL);
+	nsec = now.tv_usec * 1000 + (msec % 1000) * 1000000;
+	abstime.tv_nsec = nsec % 1000000000;
+	abstime.tv_sec = now.tv_sec + nsec / 1000000000 + msec / 1000;
+
 	pthread_mutex_lock(&event->mutex);
 	{
-		struct timespec to;
-
-		to.tv_sec = time(NULL) + sec;
-		to.tv_nsec = nsec;
-
-		pthread_cond_timedwait(&event->cond, &event->mutex, &to);
+		rcode = pthread_cond_timedwait(&event->cond, &event->mutex, &abstime);
 	}
 	pthread_mutex_unlock(&event->mutex);
+#endif
+
+	return rcode;
 }
--- Server/Common/Event.h
+++ Server/Common/Event.h
@@ -1,33 +1,31 @@
 #pragma once
-
-
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "pthread.h"
-#include "semaphore.h"
-#include "Common.h"
+
+#if defined _PLATFORM_WINDOWS_
+#include <windows.h>
+#endif
 
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+struct event_t {
+#if defined _PLATFORM_WINDOWS_
+	HANDLE hEvent;
+#else
+	int flag;
+	pthread_cond_t cond;
+	pthread_mutex_t mutex;
+#endif
+};
 
-	typedef struct {
-		BOOL flag;                                                                                 // 触发标志
-		pthread_cond_t cond;                                                                       // 条件量
-		pthread_mutex_t mutex;                                                                     // 互斥量
-	} event_t;
-
-
-	_ServerExport void event_init(event_t *event);                                                 // 初始化
-	_ServerExport void event_destroy(event_t *event);                                              // 销毁
-	_ServerExport void event_set(event_t *event);                                                  // 触发
-	_ServerExport void event_reset(event_t *event);                                                // 重置
-	_ServerExport void event_wait(event_t *event);                                                 // 等待
-	_ServerExport void event_timedwait(event_t *event, int sec, int nsec);                         // 等待
-
+extern void event_init(event_t *event);
+extern void event_destroy(event_t *event);
+extern void event_set(event_t *event);
+extern void event_reset(event_t *event);
+extern void event_wait(event_t *event);
+extern int event_timedwait(event_t *event, int msec);
 
 #ifdef __cplusplus
 }
